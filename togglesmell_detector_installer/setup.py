05a82c65f2098cac3b56a27ea972a0e7b6811a1b
Govardhan Rathamsetty <52001690+Rathamsettygovardhan@users.noreply.github.com>
2023-11-29 13:31:50 -0500

    Update setup.py

--- togglesmell_detector_installer/setup.py
+++ togglesmell_detector_installer/setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'togglesmell_detector_installer = my_package.nested-toggle-extraction:nested_toggle',
+            'togglesmell_detector_installer = my_package.nested-toggle-extraction.py:nested_toggle',
         ],
     },
 )
