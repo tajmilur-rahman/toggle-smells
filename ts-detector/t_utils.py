3a7ee8315dd0c31ba4fcf634673b9f27ddabe526
mfei-va <mzhfei@outlook.com>
2024-03-26 10:28:46 -0600

    commnet

--- ts-detector/t_utils.py
+++ ts-detector/t_utils.py
@@ -224,6 +224,7 @@ def extract_enum_toggles(code_files, t_config_files, lang):
     return []
 
 
+# WIP 
 def extract_combinatory_toggles(code_files, t_config_files, lang):
     toggle_names = get_toggles_from_config_files(lang, t_config_files)
 
