f5ec2af46bd2710d06ac0ea21d6272789d7167d0
mef382 <MZHFEI@OUTLOOK.COM>
2024-10-01 23:35:00 -0600

    filter down the list

--- ts-detector/getToggleFromFile.py
+++ ts-detector/getToggleFromFile.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 regexes = {
     'python': {
@@ -56,21 +57,45 @@ regexes = {
     }
 }
 
+language_keywords = {
+    'python': ['__main__', 'True', 'False', 'None', 'async', 'await', 'self', '"true"', '"false"'],
+    'csharp': ['public', 'private', 'protected', 'const', 'static', 'readonly', 'string', 'Dictionary', 'List', 'bool', '"true"', '"false"'],
+    'java': ['public', 'private', 'protected', 'static', 'final', 'volatile', 'transient', 'String', 'Map', 'List', 'boolean', '"true"', '"false"'],
+    'golang': ['var', 'const', 'func', 'int', 'string', 'bool', 'map', '"true"', '"false"'],
+    'cpp': ['const', 'static', 'public', 'private', 'protected', 'bool', 'int', 'float', 'double', 'std', '"true"', '"false"']
+}
+
+def is_too_short(toggle: str):
+    return len(toggle) <= 3
+def is_pure_number_or_dash_underscore(toggle):
+    return bool(re.fullmatch(r'"?\d+([-_\.]\d+)*"?', toggle))
+
+def filter_substrings(toggles):
+    toggles = sorted(toggles, key=len, reverse=True)
+    filtered_toggles = []
+    for toggle in toggles:
+        if not any(toggle in larger_toggle for larger_toggle in filtered_toggles):
+            filtered_toggles.append(toggle)
+    return filtered_toggles
+
+def filter_toggles(toggles, language):
+    keywords = language_keywords.get(language, [])
+    filtered_toggles = [t for t in toggles if not is_pure_number_or_dash_underscore(t)]
+    filtered_toggles = [t for t in filtered_toggles if len(t) > 3]
+    filtered_toggles = [t for t in filtered_toggles if t not in keywords]
+    filtered_toggles = filter_substrings(filtered_toggles)
+    return filtered_toggles
+
 def apply_combined_regexes(combined_content, language):
     toggles = set()
-
     patterns = regexes.get(language)
-
     if patterns:
-        # Apply each regex and collect results
         for pattern in patterns.keys():
             compiled_pattern = re.compile(patterns[pattern])
             matches = compiled_pattern.finditer(combined_content)
             for match in matches:
                 toggle = match.group('toggle')
-                print(match.group('toggle'))
                 toggles.add(toggle)
-
     return toggles
 
 def get_language_from_extension(file_path):
@@ -92,16 +117,13 @@ def extract_toggles_from_config_files(config_files):
         with open(conf_file, 'r') as file:
             file_content = file.read()
             toggle_list.append(file_content)
-
     toggle_list = list(filter(None, toggle_list))
     combined_content = "\n".join(toggle_list)
-
     if config_files:
         language = get_language_from_extension(config_files[0])
-
         toggles = apply_combined_regexes(combined_content, language)
-        return list(toggles)
-
+        filtered_toggles = filter_toggles(list(toggles), language)
+        return filtered_toggles
     return []
 
 # Example usage
